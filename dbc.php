719cb04a439bf56081f3aea5eec46c0c66753541
ohashi-yohei@fujibuturyu.co.jp <hashi@MacBook-Pro-2.local>
2024-09-07 17:15:40 +0900

    ブログアプリ作成 #05
    - 関数（引数・返り値）をマスターしよう！

--- dbc.php
+++ dbc.php
@@ -1,25 +1,62 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=study_blog_app;charset=utf8';
-$user = 'blog_user';
-$pass = 'Hashi123';
+// 関数一つに一つの機能のみを持たせる
+// 1. データベース接続
+// 2. データを取得する
+// 3. カテゴリ名を表示する
 
+// 1. データベース接続
+// 引数： なし
+// 返り値： 接続結果を返す
+function dbConnect()
+{
+    $dsn = 'mysql:host=localhost;dbname=study_blog_app;charset=utf8';
+    $user = 'blog_user';
+    $pass = 'Hashi123';
 
-try {
-    $dbh = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
-    // echo '接続成功<br>';
+    try {
+        $dbh = new PDO($dsn, $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        ]);
+    } catch (PDOException $e) {
+        echo "接続失敗<br>{$e->getMessage()}";
+        exit();
+    };
+
+    return $dbh;
+}
+
+// 2. データを取得する
+// 引数： なし
+// 返り値： 取得したデータ
+function getAllBlog()
+{
+    $dbh = dbConnect();
     // ①SQL文の準備
     $sql = 'SELECT * FROM blog';
     // ②SQLの実行
     $stmt = $dbh->query($sql);
     // ③SQLの結果を受け取る
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
     $dbh = null;
-} catch (PDOException $e) {
-    echo "接続失敗<br>{$e->getMessage()}";
-    exit();
+}
+
+// 取得したデータを表示
+$blogData = getAllBlog();
+
+// 3. カテゴリ名を表示する
+// 引数： 数字
+// 返り値： カテゴリーの文字列
+function setCategoryName($category)
+{
+    if ($category === '1') {
+        return 'ブログ';
+    } elseif ($category === '2') {
+        return 'ブログ';
+    } else {
+        return 'その他';
+    }
 }
 
 ?>
@@ -41,11 +78,11 @@ try {
             <th>タイトル</th>
             <th>カテゴリ</th>
         </tr>
-        <?php foreach ($result as $column): ?>
+        <?php foreach ($blogData as $column): ?>
             <tr>
                 <td><?php echo $column['id'] ?></td>
                 <td><?php echo $column['title'] ?></td>
-                <td><?php echo $column['category'] ?></td>
+                <td><?php echo setCategoryName($column['category']) ?></td>
             </tr>
         <?php endforeach; ?>
     </table>
